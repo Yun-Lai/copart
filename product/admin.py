53b98c8b997b3e5aa0fe95c970e67f3c5e1f5c57
zaza316614@gmail.com <copart.gitlab@gmail.com>
2018-10-03 15:56:58 -0400

    fix issue of slow speed when enter detail add LargeTablePaginator

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,37 @@
 from django.contrib import admin
+# from django.core.paginator import Paginator
+# from django.db import connection
 
 from product.models import *
 from product.filters import DescriptionFilter, MultipleChoicesFieldListFilter, MultipleChangeList, SourceFilter, SoldFilter
+
+
+# class LargeTablePaginator(Paginator):
+#     """
+#     Warning: Postgresql only hack
+#     Overrides the count method of QuerySet objects to get an estimate instead of actual count when not filtered.
+#     However, this estimate can be stale and hence not fit for situations where the count of objects actually matter.
+#     """
+#
+#     def _get_count(self):
+#         if getattr(self, '_count', None) is not None:
+#             return self._count
+#
+#         query = self.object_list.query
+#         if not query.where:
+#             try:
+#                 cursor = connection.cursor()
+#                 cursor.execute("SELECT reltuples FROM pg_class WHERE relname = %s",
+#                                [query.model._meta.db_table])
+#                 self._count = int(cursor.fetchone()[0])
+#             except:
+#                 self._count = super(LargeTablePaginator, self)._get_count()
+#         else:
+#             self._count = super(LargeTablePaginator, self)._get_count()
+#
+#         return self._count
+#
+#     count = property(_get_count)
 
 
 class VehicleMakesAdmin(admin.ModelAdmin):
@@ -24,6 +54,10 @@ class VehicleAdmin(admin.ModelAdmin):
     search_fields = ['name', 'vin', 'lot', 'year', 'make', 'model']
 
     readonly_fields = ['source_', 'images_', 'thumb_images_']
+
+    raw_id_fields = ['foregoing']
+
+    # paginator = LargeTablePaginator
 
     fieldsets = [
         ('', {'fields': ['vin', 'lot', 'name', 'make', 'model', 'year', 'location', 'source_']}),
