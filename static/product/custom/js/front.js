4edc2ba853086ead0e5dfa5cebc24c0ee7194857
zaza316614@gmail.com <copart.gitlab@gmail.com>
2018-10-03 14:27:15 -0400

    change static files path

--- static/product/custom/js/front.js
+++ static/product/custom/js/front.js
@@ -42,10 +42,10 @@ function front_landing_event_proc_funcs()
     // event extend and pull in
     jQuery(".f_arrival_extend_img").click(function(){
        if ( jQuery(this).attr("state") == "0" ) {
-           jQuery(this).attr("state", "1").attr("src", "static/product/assets/custom/imgs/top.png");
+           jQuery(this).attr("state", "1").attr("src", "static/product/custom/imgs/top.png");
            jQuery(".f_land_hide_arrival").removeClass("g_none_dis");
        } else {
-           jQuery(this).attr("state", "0").attr("src", "static/product/assets/custom/imgs/down.png");
+           jQuery(this).attr("state", "0").attr("src", "static/product/custom/imgs/down.png");
            jQuery(".f_land_hide_arrival").addClass("g_none_dis");
        }
     });
